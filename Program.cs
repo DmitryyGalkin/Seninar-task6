79b29da5d224f94d727d017fbcf9999d6fd3f2a3
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-09-16 15:57:59 +0300

    task6 seminar

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+string input = Console.ReadLine()!;
+int number = int.Parse(input);
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+        Console.WriteLine("Нет");
+}
