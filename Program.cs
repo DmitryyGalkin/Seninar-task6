79922ec0f27d86967653d24549fe85f520d982fd
Galkin Dmitry <dimitriusgalkin@gmail.com>
2022-09-16 22:10:47 +0300

    выравнил код

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@ Console.WriteLine("Введите число: ");
 string input = Console.ReadLine()!;
 int number = int.Parse(input);
 
-if(number % 2 == 0)
+if (number % 2 == 0)
 {
     Console.WriteLine("Да");
 }
 else
 {
-        Console.WriteLine("Нет");
+    Console.WriteLine("Нет");
 }
